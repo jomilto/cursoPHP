ea90a9bd535740b7015cdb0179e233e9ab369e89
JLob Lap <joselobatof@gmail.com>
2020-08-04 22:55:17 -0500

    namespaces

--- jobs.php
+++ jobs.php
@@ -3,6 +3,10 @@
 require_once('app/Models/Job.php');
 require_once('app/Models/Project.php');
 require_once('app/Models/Printable.php');
+require_once('lib1/Project.php');
+
+use App\Models\Job;
+use App\Models\Project;
 
 // las interfaces sirven para "validar", que el dato que se envia,
 // a las funciones contenga ciertos metodos declarados o tipos de datos
@@ -18,6 +22,8 @@ $job3 = new Job('','Undefined job!!!!');
 $job3->months = 4;
 
 $project1 = new Project('Project 1', 'First Project');
+
+$projectLib = new Lib1\Project();
 
 $jobs = [
   $job1,
