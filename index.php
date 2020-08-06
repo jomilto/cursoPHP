50848a0b8b5c26390ec9fe5a4d81cf486a7df768
JLob Lap <joselobatof@gmail.com>
2020-08-05 23:54:34 -0500

    Preparando para agregar router

--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
   // Incluyelo si lo encuentras sino manda warning
-  
-  require_once('vendor/autoload.php');
-  
-  use Illuminate\Database\Capsule\Manager as Capsule;
-  
-  $capsule = new Capsule;
-  
-  $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cursophp',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-    ]);
     
-    // Make this Capsule instance available globally via static methods... (optional)
-    $capsule->setAsGlobal();
-    
-    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-    $capsule->bootEloquent();
-    
-    include_once("jobs.php");
-    
-    // require si no encuentra el archivo detiene la ejecución
-    // con require o include se ejecuta el código cada vez que se manda a llamar
+  include_once("jobs.php");
+
+  // require si no encuentra el archivo detiene la ejecución
+  // con require o include se ejecuta el código cada vez que se manda a llamar
   // si se usa _once, solo lo incluye una vez
 
   $lastName = 'My lastname';
---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_error',1);
+    error_reporting(E_ALL);
+
+    require_once('../vendor/autoload.php');
+
+    use Illuminate\Database\Capsule\Manager as Capsule;
+  
+    $capsule = new Capsule;
+    
+    $capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'cursophp',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]);
+        
+        // Make this Capsule instance available globally via static methods... (optional)
+    $capsule->setAsGlobal();
+    
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+    $capsule->bootEloquent();
+
+    $route = $_GET['route'] ?? '/';
+
+    if ($route == '/'){
+        require_once('../index.php');
+    }elseif($route == 'addJob'){
+        require_once('../addJob.php');
+    }elseif($route == 'addProject'){
+        require_once('../addProject.php');
+    }
+    
