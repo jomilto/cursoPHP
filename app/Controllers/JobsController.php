971b067334afc3a5046ffc49e807e0f12dc05247
JLob Lap <joselobatof@gmail.com>
2020-08-08 00:21:38 -0500

    Agregando validator

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\{Job};
+use Respect\Validation\Validator;
 
 class JobsController extends BaseController {
     public function index(){
@@ -10,16 +11,31 @@ class JobsController extends BaseController {
     }
     public function add($request)
     {   
+        $responseMessage = '';
+        $responseType = '';
         //Obtenemos el metodo
         $metodo = $request->getMethod();
         // Obtenemos los datos
-
         $data = $request->getParsedBody();
-        $job = new Job();
-        $job->title = $data['title'];
-        $job->description = $data['description'];
-        $job->save();
 
-        return  $this->renderHTML('addJob.twig');
+        $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
+                                 ->key('description', Validator::stringType()->notEmpty());
+
+        if($jobValidator->validate($data)){
+            $job = new Job();
+            $job->title = $data['title'];
+            $job->description = $data['description'];
+            $job->save();
+            $responseMessage = 'Job saved';
+            $responseType = 'success';
+        }else{
+            $responseMessage = 'Job cant be saved';
+            $responseType = 'danger';
+        }
+
+        return  $this->renderHTML('addJob.twig',[
+            'responseMessage' => $responseMessage,
+            'responseType' => $responseType
+        ]);
     }
 }
