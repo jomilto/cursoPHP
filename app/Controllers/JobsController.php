9a130bdaa7972a96fcc81320698329d12aa87992
JLob Lap <joselobatof@gmail.com>
2020-08-08 01:00:48 -0500

    adding files to form

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -18,17 +18,33 @@ class JobsController extends BaseController {
         // Obtenemos los datos
         $data = $request->getParsedBody();
 
+        
+
+        
+
         $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
-                                 ->key('description', Validator::stringType()->notEmpty());
+                                 ->key('description', Validator::stringType()->notEmpty())
+                                 ->key('logo', Validator::image());
 
         if($jobValidator->validate($data)){
             $job = new Job();
             $job->title = $data['title'];
             $job->description = $data['description'];
             $job->save();
+
+            $files = $request->getUploadedFiles();
+            $logo = $files['logo'];
+
+            if($logo->getError() == UPLOAD_ERROR_OK){
+                $fileName = $logo->getClientFilename();
+                $logo->moveTo("uploads/$fileName");
+            }
+
             $responseMessage = 'Job saved';
             $responseType = 'success';
         }else{
+            // https://respect-validation.readthedocs.io/en/2.0/feature-guide/
+            // para mejor validación ^
             $responseMessage = 'Job cant be saved';
             $responseType = 'danger';
         }
