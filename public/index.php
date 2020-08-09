09c52e1714188f581128a17b0fbc73e62b336395
JLob Lap <joselobatof@gmail.com>
2020-08-09 14:56:24 -0500

    CAMBIO DE HOST

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@
     
     $capsule->addConnection([
         'driver'    => $_ENV['DB_DRIVER'],
-        'host'      => $_ENV['DATABASE_URL'],
+        'host'      => $_ENV['DB_HOST'],
         'database'  => $_ENV['DB_NAME'],
         'username'  => $_ENV['DB_USER'],
         'password'  => $_ENV['DB_PWD'],
