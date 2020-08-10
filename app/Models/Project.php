a0bd1bdc31aee263b6a9d0d6765cda1be68ffdb5
JLob Lap <joselobatof@gmail.com>
2020-08-09 19:14:49 -0500

    Add Trait to projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasDefaultImage;
 
 // require_once('BaseElement.php');
 
 class Project extends Model{
+    use HasDefaultImage;
 
     protected $table = "projects";
 
@@ -19,7 +21,7 @@ class Project extends Model{
                     <h5>{$this->title}</h5>
                     <div class=\"row\">
                         <div class=\"col-3\">
-                            <img id=\"profile-picture\" src=\"https://ui-avatars.com/api/?name=John+Doe&size=255\" alt=\"\">
+                            <img id=\"profile-picture\" src=\"{$this->getImage()}\" alt=\"\">
                         </div>
                         <div class=\"col\">
                             <p>{$this->description}</p>
