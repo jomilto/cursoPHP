7c435ace3109f1ce99453057cb3e5fbc780d6835
JLob Lap <joselobatof@gmail.com>
2020-08-14 22:23:31 -0500

    form contact

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controllers;
 
+use Zend\Diactoros\ServerRequest;
+
 class ContactController extends BaseController{
     public function index()
     {
         return $this->renderHTML('contacts/index.twig');
+    }
+
+    public function send(ServerRequest $request)
+    {
+        var_dump($request->getParsedBody());
     }
 }
