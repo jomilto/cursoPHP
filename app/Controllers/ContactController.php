f798950c37dc77366d11a91b9fa45beaf93954e0
JLob Lap <joselobatof@gmail.com>
2020-08-14 23:07:56 -0500

    Se agrega control por correo electronico

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use Zend\Diactoros\ServerRequest;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 
 class ContactController extends BaseController{
     public function index()
@@ -12,6 +15,24 @@ class ContactController extends BaseController{
 
     public function send(ServerRequest $request)
     {
-        var_dump($request->getParsedBody());
+        $data=$request->getParsedBody();
+
+        $transport = (new Swift_SmtpTransport($_ENV['SMTP_HOST'], $_ENV['SMTP_HOST']))
+            ->setUsername($_ENV['SMTP_USER'])
+            ->setPassword($_ENV['SMTP_PASSWORD']);
+
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        // Create a message
+        $message = (new Swift_Message('SomeBody Contact you'))
+        ->setFrom(['contact@mail.com' => 'Contact Form'])
+        ->setTo([$data['email'], $data['email'] => $data['name']])
+        ->setBody($data['message']);
+
+        // Send the message
+        $result = $mailer->send($message);
+
+        return $this->redirectHTML('contact');   
     }
 }
